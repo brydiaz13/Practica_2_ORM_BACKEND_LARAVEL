c43874194f629c6be18749238821db8b3b4e2e66
Brayan Diaz <bdiaz@micorreo.upp.edu.mx>
2024-09-26 23:38:36 -0600

    final

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ use App\Models\Location;
 use App\Models\Image;
 use App\Models\Category;
 use App\Models\Tag;
-use App\Models\Post; // Corregido de Pots a Post
+use App\Models\Post; 
 use App\Models\Video;
 use App\Models\Comment;
 use Illuminate\Database\Seeder;
@@ -23,15 +23,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Sembrar grupos
+  
         Group::factory(3)->create();
 
-        // Sembrar niveles
+
         Level::factory(3)->create(['name' => 'Oro']);
         Level::factory(3)->create(['name' => 'Plata']);
         Level::factory(3)->create(['name' => 'Bronce']);
 
-        // Sembrar usuarios con perfiles y otros datos relacionados
+        
         User::factory(5)->create()->each(function ($user) {
             $profile = $user->profile()->save(Profile::factory()->make());
             $profile->location()->save(Location::factory()->make());
@@ -43,13 +43,13 @@ class DatabaseSeeder extends Seeder
             ]));
         });
 
-        // Sembrar categorías
+        
         Category::factory(5)->create();
 
-        // Sembrar etiquetas
+        
         Tag::factory(12)->create();
 
-        // Sembrar publicaciones con imágenes y comentarios
+        
         Post::factory(40)->create()->each(function ($post) {
             $post->image()->save(Image::factory()->make());
             $post->tags()->attach($this->array(rand(1, 12)));
@@ -61,7 +61,7 @@ class DatabaseSeeder extends Seeder
             }
         });
 
-        // Sembrar videos con imágenes y comentarios
+        
         Video::factory(40)->create()->each(function ($video) {
             $video->image()->save(Image::factory()->make());
             $video->tags()->attach($this->array(rand(1, 12)));
@@ -77,7 +77,7 @@ class DatabaseSeeder extends Seeder
         
     }
     /**
-     * Crea un array con valores del 1 al máximo especificado.
+     * 
      *
      * @param int $max
      * @return array
--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,33 @@
 
 use App\Models\Level;
 use Illuminate\Support\Facades\Route;
-use App\Models\User; // Importa el modelo correcto
+use App\Models\User;
 
 
-// Ruta para la página principal
 Route::get('/', function () {
-    $users = User::all(); // Usa all() para obtener todos los registros
+    $users = User::all(); 
     
     return view('welcome', ['users' => $users]);
 });
 
-// Ruta para el perfil de usuario
+
 Route::get('/profile/{id}',function($id){
 
-    // Encuentra el usuario por ID
+ 
     $user = User::find($id);
 
-    // Obtiene los posts del usuario y cuenta los comentarios asociados
+    
     $posts = $user->posts()->with('category','image','tags')
     ->withCount('comments')->get();
     
 
-    // Obtiene los videos del usuario y cuenta los comentarios asociados
+    
     $videos = $user->videos()->with('category','image','tags')
     ->withCount('comments')->get();
 
-    // Devuelve la vista 'profile' con los datos
+  
     return view('profile',[
-        'user' => $user, // Faltaba una coma aquí
+        'user' => $user, 
         'posts' => $posts,
         'videos' => $videos
     ]);
@@ -38,21 +37,21 @@ Route::get('/profile/{id}',function($id){
 
 Route::get('/level/{id}',function($id){
 
-    // Encuentra el usuario por ID
+  
     $level = Level::find($id);
 
-    // Obtiene los posts del usuario y cuenta los comentarios asociados
+   
     $posts = $level->posts()->with('category','image','tags')
     ->withCount('comments')->get();
     
 
-    // Obtiene los videos del usuario y cuenta los comentarios asociados
+    
     $videos = $level->videos()->with('category','image','tags')
     ->withCount('comments')->get();
 
-    // Devuelve la vista 'profile' con los datos
+    
     return view('level',[
-        'level' => $level, // Faltaba una coma aquí
+        'level' => $level, 
         'posts' => $posts,
         'videos' => $videos
     ]);
